0fa9da977c5147e4c45918fef9ba3c2a1b0588e9
Slawomir Jaranowski <s.jaranowski@gmail.com>
2025-10-12 13:15:20 +0200

    Site checkin for project Plexus Velocity Component

---
+++ apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.codehaus.plexus.velocity.internal","l":"DefaultVelocityComponent"},{"p":"org.codehaus.plexus.velocity","l":"VelocityComponent"},{"p":"org.codehaus.plexus.velocity","l":"VelocityComponentConfigurator"}];updateSearchResults();
